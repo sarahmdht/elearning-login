1a84be83d92a1bad3aaf87ed62669326615422cd
sarahmdht <sarahmdht@gmail.com>
2023-12-12 12:53:03 +0200

    session 12

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { LogInComponent } from './log-in/log-in.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,LogInComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
